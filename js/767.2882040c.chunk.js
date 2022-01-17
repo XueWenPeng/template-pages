86f2734d5b52555a8e0d86ec40a09bc7d00a56af
unknown <wenpeng.xue@sky-mobi.com>
2022-01-17 15:06:07 +0800

    build

---
+++ js/767.2882040c.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmishuweb_tool=self.webpackChunkmishuweb_tool||[]).push([[767],{9767:function(e,t,u){u.r(t);var l=u(9301),n=u(6035);t.default=function(){return l.createElement("div",null,"Page2",l.createElement(n.OL,{to:"/template-pages/"},"home"))}}}]);
